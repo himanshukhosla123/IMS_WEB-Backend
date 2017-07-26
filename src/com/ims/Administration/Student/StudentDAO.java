0a46def1065b42b5cc05dfe55bb1eb25974eae4c
sanuj123 <sanujs97@gmail.com>
2017-07-26 23:28:55 +0530

    Add files via upload

--- src/com/ims/Administration/Student/StudentDAO.java
+++ src/com/ims/Administration/Student/StudentDAO.java
@@ -1,5 +1,87 @@
 package com.ims.Administration.Student;
 
-public class StudentDAO {
+import static com.ims.Common.CommonDAO.IAdministratorSql.getConnection;
+import static com.ims.Common.sqlQueries.ISqlQueries.ADD_STUDENT;
+import static com.ims.Common.sqlQueries.ISqlQueries.READ_STUDENT;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class StudentDAO {
+    public int addStudent(StudentDTO studentDTO) throws ClassNotFoundException, SQLException {
+    	Connection con = null;
+    	PreparedStatement psmt = null;
+    	int recCount;
+    	try{
+    		con = getConnection();
+    		con.setAutoCommit(false);
+    		psmt = con.prepareStatement(ADD_STUDENT);
+    		psmt.setInt(1,studentDTO.getSid());
+    		psmt.setString(2,studentDTO.getName());
+    		psmt.setInt(3,studentDTO.getAge());
+    		psmt.setString(4,studentDTO.getSex());
+    		psmt.setInt(5,studentDTO.getPhoneNo());
+    		psmt.setString(6,studentDTO.getEmail());
+    		psmt.setString(7, studentDTO.getPhotoPath());
+    		psmt.setDate(8,new java.sql.Date(studentDTO.getDob().getTime()));
+    		psmt.setString(9,studentDTO.getFatherName());
+    		psmt.setInt(10,studentDTO.getFatherPhNo());
+    		psmt.setString(11,studentDTO.getAddress());
+    		psmt.setDate(12,new java.sql.Date(studentDTO.getDateOfJoin().getTime()));
+    		psmt.setString(13,studentDTO.getStatus());
+
+    	    recCount = psmt.executeUpdate();
+    		if(recCount>0) {
+    			con.commit();
+    		}
+    		else {
+    			con.rollback();
+    		}
+    	}
+    	finally {
+			if(psmt!=null) {
+				psmt.close();
+			}
+			if(con!=null) {
+				con.close();
+			}
+		}
+    	return recCount;
+    	
+    }
+    
+    public ArrayList<StudentDTO> readStudent() throws ClassNotFoundException, SQLException {
+      ArrayList<StudentDTO> studentList = new ArrayList<>();	
+      Connection con = null;
+      PreparedStatement psmt = null;
+      ResultSet rs = null;
+      try {
+    	  con = getConnection();
+    	  con.setAutoCommit(false);
+    	  psmt = con.prepareStatement(READ_STUDENT);
+    	  rs = psmt.executeQuery();
+    	  while(rs.next()) {
+    		 StudentDTO stud = new StudentDTO(rs.getInt(1),rs.getString(2),rs.getInt(3)
+    				                            ,rs.getString(4),rs.getInt(5),rs.getString(6),rs.getString(7),rs.getDate(8)
+    				                            ,rs.getString(9),rs.getInt(10),rs.getString(11),rs.getDate(12),rs.getString(13));
+    	  
+    	  studentList.add(stud);
+    	  }
+      }
+      finally {
+    	  if(rs!=null)
+    		  rs.close();
+    	  if(psmt!=null)
+    		  psmt.close();
+    	  if(con!=null)
+    		  con.close();
+		
+	}
+	return studentList ;
+    }
 }
+    
--- src/com/ims/Administration/Student/StudentDTO.java
+++ src/com/ims/Administration/Student/StudentDTO.java
@@ -1,5 +1,200 @@
 package com.ims.Administration.Student;
 
+import java.util.Date;
+
 public class StudentDTO {
+    private int Sid;
+    private String name;
+    private int age;
+    private String sex;
+    private int phoneNo;
+    private String email;
+    private String photoPath;
+    private Date dob;
+    private String fatherName;
+    private int fatherPhNo;
+    private String address;
+    private Date dateOfJoin;
+    private String status;
+
+public StudentDTO() {
+	
+}
+
+@Override
+public String toString() {
+	return "StudentDTO [Sid=" + Sid + ", name=" + name + ", age=" + age + ", sex=" + sex + ", phoneNo=" + phoneNo
+			+ ", email=" + email + ", photoPath=" + photoPath + ", dob=" + dob + ", fatherName=" + fatherName
+			+ ", fatherPhNo=" + fatherPhNo + ", address=" + address + ", dateOfJoin=" + dateOfJoin + ", status="
+			+ status + "]";
+}
+
+public StudentDTO(int sid, String name, int age, String sex, int phoneNo, String email, String photoPath, Date dob, String fatherName,
+		int fatherPhNo, String address, Date dateOfJoin, String status) {
+	
+	Sid = sid;
+	this.name = name;
+	this.age = age;
+	this.sex = sex;
+	this.phoneNo = phoneNo;
+	this.email = email;
+	this.photoPath = photoPath;
+	this.dob = dob;
+	this.fatherName = fatherName;
+	this.fatherPhNo = fatherPhNo;
+	this.address = address;
+	this.dateOfJoin = dateOfJoin;
+	this.status = status;
+}
+
+public int getSid() {
+	return Sid;
+}
+
+
+
+public void setSid(int sid) {
+	Sid = sid;
+}
+
+
+
+public String getName() {
+	return name;
+}
+
+
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+
+public int getAge() {
+	return age;
+}
+
+
+
+public void setAge(int age) {
+	this.age = age;
+}
+
+
+
+public String getSex() {
+	return sex;
+}
+
+
+
+public void setSex(String sex) {
+	this.sex = sex;
+}
+
+
+
+public int getPhoneNo() {
+	return phoneNo;
+}
+
+
+
+public void setPhoneNo(int phoneNo) {
+	this.phoneNo = phoneNo;
+}
+
+
+
+public String getEmail() {
+	return email;
+}
+
+
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+
+
+public Date getDob() {
+	return dob;
+}
+
+
+
+public void setDob(Date dob) {
+	this.dob = dob;
+}
+
+
+
+public String getFatherName() {
+	return fatherName;
+}
+
+
+
+public void setFatherName(String fatherName) {
+	this.fatherName = fatherName;
+}
+
+
+
+public int getFatherPhNo() {
+	return fatherPhNo;
+}
+
+
+
+public void setFatherPhNo(int fatherPhNo) {
+	this.fatherPhNo = fatherPhNo;
+}
+
+
+
+public String getAddress() {
+	return address;
+}
+
+
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+
+
+public Date getDateOfJoin() {
+	return dateOfJoin;
+}
+
+
+
+public void setDateOfJoin(Date dateOfJoin) {
+	this.dateOfJoin = dateOfJoin;
+}
+
+
+
+public String getStatus() {
+	return status;
+}
+
+
+
+public void setStatus(String status) {
+	this.status = status;
+}
+
+public String getPhotoPath() {
+	return photoPath;
+}
+
+public void setPhotoPath(String photoPath) {
+	this.photoPath = photoPath;
+}
 
 }
