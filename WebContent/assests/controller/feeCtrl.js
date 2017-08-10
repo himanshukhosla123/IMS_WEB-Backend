e947d216592039cb7714056f4d0446480424f1cf
sOO_sOOry <hitesh6sharma@gmail.com>
2017-08-10 18:16:40 +0530

    00

--- WebContent/assests/controller/feeCtrl.js
+++ WebContent/assests/controller/feeCtrl.js
@@ -6,25 +6,18 @@ app.controller("feeCtrl", function ($scope, feeFactory) {
 	
 	$scope.manageAddorUpdateFee = function () {
 		var obj = $scope.fee;
-		console.log("manage");
-		console.log($scope.doDisable);
-		console.log($scope.addMode);
-	    if ($scope.addMode === true) {
+	    if ($scope.doDisable === false && $scope.addMode === true) {
             $scope.addFee(obj);
-            console.log("calling add fee");
 	    } else if ($scope.doDisable === false && $scope.addMode === false) {
-	    	$scope.updateFee(obj);
-	    	console.log("Calling update fee");
+            $scope.updateFee(obj);
 	    }
 	};
 	
     $scope.addFee = function (obj) {
         console.log(obj);
         var promise = feeFactory.addFee(obj);
-        console.log("add");
         promise.then(function (data) {
             $scope.feeList.push(data.data);
-            console.log($scope.fee);
         }, function (er) {
             $scope.error = er;
         });
@@ -33,7 +26,6 @@ app.controller("feeCtrl", function ($scope, feeFactory) {
     $scope.readAllFee = function () {
 		var promise = feeFactory.readAllFee();
 		promise.then(function (data) {
-            console.log(data.data);
 			$scope.feeList = data.data;
 		}, function (er) {
 			$scope.error = er;
@@ -45,10 +37,9 @@ app.controller("feeCtrl", function ($scope, feeFactory) {
         var promise = feeFactory.readFee(obj);
         $scope.doDisable = doDisable;
         promise.then(function (data) {
-            $scope.fee=data.data;
+            $scope.fee = data.data;
             for (x in $scope.fee) {
                 $scope.fee[x] = data.data[x];
-                console.log($scope.fee[x]);
             }
 		}, function (er) {
 			$scope.error = er;
@@ -58,9 +49,9 @@ app.controller("feeCtrl", function ($scope, feeFactory) {
     $scope.updateFee = function (obj) {
         var promise = feeFactory.updateFee(obj);
         promise.then(function (data) {
-            $scope.fee=data.data;
+            $scope.fee = data.data;
             for (x in $scope.fee) {
-            	$scope.feeList[0] = data.data;
+                $scope.feeList[0] = data.data;
             }
 		}, function (er) {
 			$scope.error = er;
@@ -71,19 +62,28 @@ app.controller("feeCtrl", function ($scope, feeFactory) {
         var obj = $scope.feeList[index];
         var promise = feeFactory.deleteFee(obj);
         promise.then(function (data) {
-            $scope.feeList.splice(index,1);
+            $scope.feeList.splice(index, 1);
 		}, function (er) {
 			$scope.error = er;
 		});
     };
 
     $scope.resetFeilds = function () {
-    	$scope.doDisable = false;
-    	for(x in $scope.fee){
-    		$scope.fee[x] = "";
-    	}
-    	$scope.addMode = false;
-        console.log($scope.doDisable);
+        $scope.doDisable = false;
+        for (x in $scope.fee) {
+            $scope.fee[x] = "";
+        }
+        $scope.addMode = false;
     };
+    
+    $scope.isCheque = function () {
+        console.log("Payment Mode : ", $scope.fee["paymentMode"]);
+        if($scope.fee["paymentMode"] == "Cheque") {
+            return true;
+        }
+        else {
+            return true;
+        }
+    }
     
 });
